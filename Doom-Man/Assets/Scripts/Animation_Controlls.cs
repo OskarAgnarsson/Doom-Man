3938a5c153928068886ac64ce90d7a94b9de9660
Frillion <fridriksoebech@gmail.com>
2019-09-18 15:08:32 +0000

    Vann Meira í dodge scriptinu og bjó til animator fyrir player

---
+++ Doom-Man/Assets/Scripts/Animation_Controlls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Controlls : MonoBehaviour
+{
+    [SerializeField] public GameObject player;
+    private PlayerController playerscript;
+    private Animator playeranim;
+    
+
+    void Start()
+    {
+        playerscript = player.gameObject.GetComponent<PlayerController>();
+        playeranim = player.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerscript.is_rolling == true)
+        {
+            playeranim.SetTrigger("roll");
+        }
+        if(playerscript.is_rolling == false)
+        {
+            playeranim.ResetTrigger("roll");
+        }
+    }
+}
--- Doom-Man/Assets/Scripts/PlayerController.cs
+++ Doom-Man/Assets/Scripts/PlayerController.cs
@@ -8,19 +8,24 @@ public class PlayerController : MonoBehaviour
     public bool is_rolling;
 
     bool testvar;
+    private float original_playerspeed;
     private float mvx;
     private float mvy;
-    private float roll_time;
+    [SerializeField] private float roll_time;
+    private float original_roll_time;
+    [SerializeField] private float roll_modifier;
     private Vector2 mv;
     private Rigidbody2D playerbody;
     
 
     void Awake()
     {
-        testvar = false;
         is_rolling = false;
         playerspeed = 5f;
-        roll_time = 0.5f;
+        original_playerspeed = playerspeed;
+        roll_time = 0.3f;
+        original_roll_time = roll_time;
+        roll_modifier = 2.5f;
     }
     // Start is called before the first frame update
     void Start()
@@ -31,31 +36,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mvx = Input.GetAxis("Horizontal") * playerspeed;
-        mvy = Input.GetAxis("Vertical") * playerspeed;
-        mv = new Vector2(mvx,mvy);
+        mvInputs();
+        roll();
+    }
+
+    void FixedUpdate()
+    {
+        playerbody.MovePosition(playerbody.position +  mv *  Time.fixedDeltaTime);
+    }
+
+
+    //These Are The Inputs The Player Uses To Move
+    void mvInputs()
+    {
+        mvx = Input.GetAxis("Horizontal");
+        mvy = Input.GetAxis("Vertical") ;
+        mv = new Vector2(mvx* playerspeed,mvy* playerspeed);
+    }
+
+
+    //Allows The Player To roll and change direction while doing so
+    void roll()
+    {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            testvar = true;
-            playerspeed = 10f;
+            playerspeed = playerspeed * roll_modifier;
             is_rolling = true;
         }
         if(roll_time <= 0f)
         {
-            playerspeed = 5f;
-            roll_time = 0.5f;
+            playerspeed = original_playerspeed;
+            roll_time = original_roll_time;
             is_rolling = false;
         }
         if(roll_time > 0f && is_rolling)
         {
             roll_time -= Time.deltaTime;
         }
-    }
-
-    void FixedUpdate()
-    {
-        Debug.Log(testvar);
-        Debug.Log(mv*Time.fixedDeltaTime);
-        playerbody.MovePosition(playerbody.position +  mv *  Time.fixedDeltaTime);
     }
 }
