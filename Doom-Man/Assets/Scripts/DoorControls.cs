9459f4f794991cd9dbc5314443d3fec83c68de1d
OskarAgnarsson <oskarag309@nemi.tskoli.is>
2019-11-06 15:11:01 +0000

    Gerði byrjun á script fyrir hurðir

---
+++ Doom-Man/Assets/Scripts/DoorControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControls : MonoBehaviour
+{
+    public GameObject player;
+    public bool playerClose = false;
+
+
+    void Update()
+    {
+        if (Vector2.Distance(new Vector2(transform.position.x,transform.position.y),new Vector2(player.transform.position.x,player.transform.position.y)) < 5f) {
+            playerClose = true;
+        } else {
+            playerClose = false;
+        }
+        Debug.Log(playerClose);
+    }
+}
