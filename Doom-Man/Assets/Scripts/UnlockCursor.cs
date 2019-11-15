479cf88a922d7fd409aa393254bae0fcd678dde8
OskarAgnarsson <oskaragnarson@gmail.com>
2019-11-15 10:10:14 +0000

    Byrjaði á menu, á eftir að setja grafík fyrir menu og kóða fyrir HUD

---
+++ Doom-Man/Assets/Scripts/UnlockCursor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCursor : MonoBehaviour
+{
+    private void Awake()//aflæsir cursor
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
